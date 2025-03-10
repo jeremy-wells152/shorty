318d64a0c4d0d86c5231b73ad82f11bc9cf517d4
jeremy-wells152 <jeremyleroy96+dev@gmail.com>
2025-03-10 00:50:26 -0500

    dont use static, just serve file

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ import urlRoutes from "./routes/urlRoutes.js";
 
 const app = express();
 
-app.use(express.static("public"));
 app.use(express.json());
 app.use("/", urlRoutes);
+
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.html");
+});
 
 export default app;
